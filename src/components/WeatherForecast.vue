8932efb603e0b8495c71282e659ff331a1c5fff4
Tae Kim <taekim.dev@gmail.com>
2023-07-13 21:36:35 +0800

    forecast layout change

--- src/components/WeatherForecast.vue
+++ src/components/WeatherForecast.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="forecast-wrapper">
-    <WeatherDisplay 
-      weatherCategory="Warm" 
-      avatarImage="avatar_warm.png" 
-      weatherDescription="It's a warm day today!" 
-    />
-    <WeatherDisplay 
-      weatherCategory="Cool" 
-      avatarImage="avatar_cool.png" 
-      weatherDescription="It's a cool day tomorrow!" 
-    />
+    <div class="weather-display-container">
+      <WeatherDisplay 
+        weatherCategory="Warm" 
+        avatarImage="avatar_warm.png" 
+        weatherDescription="It's a warm day today!" 
+      />
+      <WeatherDisplay 
+        weatherCategory="Cool" 
+        avatarImage="avatar_cool.png" 
+        weatherDescription="It's a cool day tomorrow!" 
+      />
+    </div>
     <div class="more-forecast">
-      More Forecast ➡
+      5 Days Forecast ➡
     </div>
   </div>
 </template>
@@ -29,19 +31,45 @@ export default {
 
 <style scoped>
 .forecast-wrapper {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-  flex-wrap: wrap;
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px 0;
+}
+
+.weather-display-container {
+  width: 40%;
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 10px;
+}
+
+.weather-display-container weatherdisplay {
+  width: 45%;
 }
 
 .more-forecast {
+  width: 15%;
   padding: 20px;
   font-size: 1.2em;
   cursor: pointer;
-  align-self: center;
   text-align: center;
+}
+
+@media (max-width: 600px) {
+  .weather-display-container {
+    width: 100%;
+  }
+  .weather-display-container weatherdisplay {
+    width: calc(50% - 10px);
+    box-sizing: border-box;
+    margin: 0 5px;
+  }
+  .more-forecast {
+    width: 50%;
+    box-sizing: border-box;
+    margin: 0 auto;
+  }
 }
 </style>
